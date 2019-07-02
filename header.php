fee88448179291fe9407981be62cbdd702f56803
Hives <communty.hivemind@gmail.com>
2019-07-02 11:55:14 +0100

    A bunch of stuff

--- header.php
+++ header.php
@@ -20,6 +20,9 @@ $environment = WP_DEBUG == true ? "local" : "production";
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
 <?php wp_head(); ?>
+
+<?php /* loads fontawesome from CDN http://fontawesome.io/get-started/ */ ?>
+<script src="https://use.fontawesome.com/2bd5f6627d.js"></script>
 </head>
 
 <body <?php body_class(); ?>>
@@ -38,26 +41,41 @@ $environment = WP_DEBUG == true ? "local" : "production";
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'faurefold' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<div class="top">
-				<?php
-				$description = get_bloginfo( 'description', 'display' );
-				if ( $description || is_customize_preview() ) : ?>
-					<div class="site-description full-width"><?php echo $description; /* WPCS: xss ok. */ ?></div>
-				<?php
-				else : ?>
-					<div class="site-description full-width">&nbsp;</div>
-				<?php
-				endif; ?>				
+
+		<!-- pink bar across top -->
+		<div class="top">
+			<?php
+			$description = get_bloginfo( 'description', 'display' );
+			if ( $description || is_customize_preview() ) : ?>
+				<div class="site-description full-width"><?php echo $description; /* WPCS: xss ok. */ ?></div>
+			<?php
+			else : ?>
+				<div class="site-description full-width">&nbsp;</div>
+			<?php
+			endif; ?>				
+		</div>
+
+		<!-- main content of header -->
+		<div class="middle full-width clear">
+
+			<div class="title-and-logo">
+				<img class="guiding-logo" src=<?php echo get_stylesheet_directory_uri() . "/img/ggselogo.png"; ?>>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>		
 			</div>
 
-			<div class="middle full-width">
-				<img src=<?php echo get_stylesheet_directory_uri() . "/img/ggselogo.png"; ?>>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>				
+			<div class="social-media">
+				<a class="mydonate" target="_blank" href="https://mydonate.bt.com/charities/girlguidingsurreyeast-faurefold">
+					<img class="mydonate-logo" src=<?php echo get_stylesheet_directory_uri() . "/img/mydonate.jpg"; ?>>
+				</a>
+				<a class="facebook" target="_blank" href="https://www.facebook.com/Faurefold">
+					<!-- requires fontawesome to be loaded -->
+					<i class="fa fa-facebook-square" aria-hidden="true"></i>
+				</a>
 			</div>
 
-		</div><!-- .site-branding -->
+		</div>
 
+		<!-- nav bar -->
 		<div class="bottom">
 			<div class="nav-container full-width">
 				<nav id="site-navigation" class="main-navigation" role="navigation">
